9008413fe6e52eeaf97b274df31f33f5e3cee5cd
yomisimie <yomisimie@gmail.com>
2021-03-24 09:58:44 +0200

    Updated page title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { useEffect } from "react";
 import "./App.scss";
 import Layout from "./components/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 function App() {
+  useEffect(() => {
+    document.title = "Burger Builder";
+  }, []);
   return (
     <div className="App">
       <Layout>
