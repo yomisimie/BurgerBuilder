ba08f31063b4b295f3f6e2d7ac8b8a47ad0dd6d4
Silviu Apostol <yomisimie@gmail.com>
2022-11-19 22:29:22 +0200

    Updated icons, updated packages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.scss";
 import Layout from "./components/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
@@ -7,8 +7,14 @@ function App() {
   useEffect(() => {
     document.title = "Burger Builder";
   }, []);
+
+  const isBrowserDefaultDark = () =>
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+  const [theme] = useState(isBrowserDefaultDark() ? "dark" : "light");
+
   return (
-    <div className="App">
+    <div className={`App theme-${theme}`}>
       <Layout>
         <BurgerBuilder />
       </Layout>
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -4,7 +4,7 @@ import NavigationItems from "../NavigationItems/NavigationItems";
 import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
 
 const toolbar = (props) => (
-  <header>
+  <header className="header">
     <div className="container h-100">
       <div className="row text-center h-100">
         <div className="col-12 col-lg-4 h-100">
